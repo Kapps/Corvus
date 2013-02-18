40e1f7ed2ac1d769edeca748cb099d2813daea5f
Octanum <Octanum@gmail.com>
2013-02-17 22:32:29 -0600

    More stuff in movementcomponent now.

--- Corvus/CorvEngine/Entities/MovementComponent.cs
+++ Corvus/CorvEngine/Entities/MovementComponent.cs
@@ -7,6 +7,12 @@ namespace CorvEngine.Entities
 {
     public class MovementComponent : Component
     {
+        public float maxWalkVelocity = 500f;
+        public float maxJumpVelocity = 1050f;
+        public float gravity = 5000.5f;
+        public bool isJumping = false;
+        public bool isGrounded = true;
+        public bool jumpStart = false; //This flag is just essentially to account for the fact that we're grounded on the first jump. Could maybe do something like airtime too eventually.
         public Direction CurrDir = Direction.Down;
     }
 }
