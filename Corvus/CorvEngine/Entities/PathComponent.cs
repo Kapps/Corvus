5bc32bae88c308e9a07fb01e569c8b413b73f025
Octanum <Octanum@gmail.com>
2013-02-17 19:30:29 -0600

    PathComponent stuff I guess?

---
+++ Corvus/CorvEngine/Entities/PathComponent.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CorvEngine.Entities
+{
+    class PathComponent : Component
+    {
+        //Lazy implementation since not sure if permanent yet.
+
+        public List<Vector2> Nodes = new List<Vector2>();
+        public Vector2 CurrentNode;
+        public int ArrivedNode = 5;
+        int nodeIndex = 0;
+
+        public void AddNode(Vector2 node)
+        {
+            if (Nodes.Count() == 0)
+            {
+                CurrentNode = node;
+                nodeIndex = 0;
+            }
+
+            Nodes.Add(node);
+        }
+
+        public void NextNode()
+        {
+            if (CurrentNode == Nodes.Last())
+            {
+                CurrentNode = Nodes.First();
+                nodeIndex = 0;
+            }
+            else
+            {
+                CurrentNode = Nodes[nodeIndex + 1];
+            }
+        }
+    }
+}
