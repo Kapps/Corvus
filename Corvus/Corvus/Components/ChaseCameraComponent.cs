b8dee29a6cdc480abc2a7c9178633bbae5f1454c
Ognjen Ivkovic <opantm2+git@gmail.com>
2013-02-20 23:55:18 -0600

    Prevented camera going off-screen.

--- Corvus/Corvus/Components/ChaseCameraComponent.cs
+++ Corvus/Corvus/Components/ChaseCameraComponent.cs
@@ -25,7 +25,12 @@ namespace Corvus.Components {
 
 		protected override void OnUpdate(GameTime Time) {
 			var EntityMiddle = new Vector2(Parent.Location.X + (Parent.Location.Width / 2), Parent.Location.Y + (Parent.Location.Height / 2));
-			_Camera.Position = Parent.Position - (_Camera.Size / 2);
+			var CamPos = Parent.Position - (_Camera.Size / 2);
+			CamPos.X = Math.Max(CamPos.X, 0);
+			CamPos.Y = Math.Max(CamPos.Y, 0);
+			CamPos.X = Math.Min(CamPos.X, Scene.MapSize.X - _Camera.Size.X);
+			CamPos.Y = Math.Min(CamPos.Y, Scene.MapSize.Y - _Camera.Size.Y);
+			_Camera.Position = CamPos;
 			base.OnUpdate(Time);
 		}
 
