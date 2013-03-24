cc59dc12650bd29eafa3af949ab92b01bce16b71
Octanum <Octanum@gmail.com>
2013-03-23 20:09:56 -0600

    Templating level changing more.
    I also potentially broke something here because I deleted entities by accident. Readded them and everything seems to work fine. It also says nothing was changed, but just in case.

---
+++ Corvus/Corvus/Components/LevelChangeComponent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CorvEngine.Components;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using CorvEngine.Graphics;
+using CorvEngine;
+
+namespace Corvus.Components
+{
+    class LevelChangeComponent : CollisionEventComponent
+    {
+        public string PrevLevel
+        {
+            get { return _PrevLevel; }
+            set { _PrevLevel = value; }
+        }
+
+        public string NextLevel
+        {
+            get { return _NextLevel; }
+            set { _NextLevel = value; }
+        }
+
+        private string _PrevLevel;
+        private string _NextLevel;
+
+        protected override bool OnCollision(Entity Entity, EntityClassification Classification)
+        {
+            //CorvusGame.Instance.SceneManager.ChangeScene
+            return true;
+        }
+    }
+}
