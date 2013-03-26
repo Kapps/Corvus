e2fdce335d05863d5e72478fc61b87b8bd5fc04c
Octanum <Octanum@gmail.com>
2013-03-25 20:59:36 -0600

    More basic arean stuff. Changed it from CorvEngine to Corvus.

--- Corvus/Corvus/Components/ArenaSystem.cs
+++ Corvus/Corvus/Components/ArenaSystem.cs
@@ -7,6 +7,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using CorvEngine.Scenes;
 using Microsoft.Xna.Framework;
+using Corvus.Components;
 
 namespace CorvEngine.Components
 {
@@ -18,19 +19,19 @@ namespace CorvEngine.Components
         }
 
         private int _Wave;
-        private int _TotalEnemiesWave;
+        private int _TotalEntitiesSpawned;
+        private int _TotalEntitiesSpawnedWave;
 
         //Unsure about these being here, but fine for now.
-        private int _TotalEnemiesSpawned;
-        private int _TotalEnemiesKilled;
-        private int _TotalEnemiesSpawnedWave;
-        private int _TotalEnemiesKilledWave;
-
-        private int _DifficultyModifier; //This'll actually just be calculated by wave most likely, so probably unnecessary.
+        private int _TotalEntitiesKilled;
+        private int _TotalEntitiesKilledWave;
 
         protected override void OnUpdate(GameTime Time)
         {
-
+            foreach (Entity entity in Scene.Entities.Where(c => c.GetComponent<SpawnerComponent>() != null))
+            {
+                
+            }
         }
     }
 }
--- Corvus/Corvus/Components/SpawnerComponent.cs
+++ Corvus/Corvus/Components/SpawnerComponent.cs
@@ -18,18 +18,48 @@ namespace Corvus.Components
             set { _LastSpawn = value; }
         }
 
+        public int TotalEntitiesSpawned
+        {
+            get { return _TotalEntitiesSpawned; }
+            set { _TotalEntitiesSpawned = value; }
+        }
+
+        //Best to put this somewhere else?
+        public int EntitiesToSpawn
+        {
+            get { return _EntitiesToSpawn; }
+            set { _EntitiesToSpawn = value; }
+        }
+
+        public bool SpawnerEnabled
+        {
+            get { return _SpawnerEnabled; }
+            set { _SpawnerEnabled = value; }
+        }
+
+        public List<Entity> EntitiesSpawned
+        {
+            get { return _EntitiesSpawned; }
+            set { _EntitiesSpawned = value; }
+        }
+
+        private bool _SpawnerEnabled = true;
         private DateTime _LastSpawn;
-        private List<Entity> _SpawnedEntities;
-        private bool _Enabled;
+        private List<Entity> _EntitiesSpawned = new List<Entity>();
+        private int _TotalEntitiesSpawned;
+        private int _EntitiesToSpawn = 1;
+        private ArenaSystem ArenaSystem;
 
         protected override void OnUpdate(GameTime Time)
         {
-            if ((DateTime.Now - LastSpawn).TotalSeconds > 2)
+            if ((DateTime.Now - LastSpawn).TotalSeconds > 2 && TotalEntitiesSpawned != EntitiesToSpawn && SpawnerEnabled)
             {
                 Entity entity = CorvEngine.Components.Blueprints.EntityBlueprint.GetBlueprint("TestEntityEnemy").CreateEntity();
                 entity.Position = this.Parent.Position;
                 entity.Size = new Vector2(32, 32);
                 Scene.AddEntity(entity);
+                TotalEntitiesSpawned++;
+                EntitiesSpawned.Add(entity);
                 LastSpawn = DateTime.Now;
             }
 
@@ -38,7 +68,7 @@ namespace Corvus.Components
 
         protected override void OnInitialize()
         {
-
+            ArenaSystem = Scene.GetSystem<ArenaSystem>();
         }
     }
 }
