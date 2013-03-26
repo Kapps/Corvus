26668b9e2f19cd17f125271b132d2ffba8781569
Octanum <Octanum@gmail.com>
2013-03-25 19:06:05 -0600

    Template for what the arena thing may look like? Not really anything done, so easily deletable for now.

---
+++ Corvus/CorvEngine/Components/ArenaSystem.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CorvEngine.Components
+{
+    class ArenaSystem : SceneSystem
+    {
+        int _Wave;
+        int _TotalEnemiesSpawned;
+        int _DifficultModifier;
+
+        protected override void OnUpdate(GameTime Time)
+        {
+
+        }
+    }
+}
